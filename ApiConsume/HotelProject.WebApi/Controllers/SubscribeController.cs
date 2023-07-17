7a09168010b024c3091877564e4feec22063560c
adnantr <963214785Aa**>
2023-07-17 16:02:08 +0300

    frontend

---
+++ ApiConsume/HotelProject.WebApi/Controllers/SubscribeController.cs
@@ -0,0 +1,49 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribeController : ControllerBase
+    {
+        private readonly ISubscribeService _subscribeService;
+        public SubscribeController(ISubscribeService subscribeService)
+        {
+            _subscribeService = subscribeService;
+        }
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            var values = _subscribeService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddRoom(Subscribe subscribe)
+        {
+            _subscribeService.TInsert(subscribe);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom(int id)
+        {
+            var values = _subscribeService.TGetByID(id);
+            _subscribeService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom(Subscribe subscribe)
+        {
+            _subscribeService.TUpdate(subscribe);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetRoom(int id)
+        {
+            var values = _subscribeService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Controllers/TestimonialController.cs
@@ -0,0 +1,49 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            var values = _testimonialService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddRoom(Testimonial testimonial)
+        {
+            _testimonialService.TInsert(testimonial);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom(Testimonial testimonial)
+        {
+            _testimonialService.TUpdate(testimonial);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetRoom(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
